f5a072201b340527624249bcf1ffaca92e1c2e84
gsenagavarapu <gopichand.senagavarapu@metlife.com>
2021-01-05 00:29:45 +0530

    Added admin file

--- college/student/admin.py
+++ college/student/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import  (Student,Department)
+
+
+@admin.register(Student, Department)
+class CollegeAdmin(admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
--- college/student/models.py
+++ college/student/models.py
@@ -1,13 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+    department_id = models.AutoField(primary_key = True)
+    department_name = models.CharField(max_length=250, blank=False)
+
+    class Meta:
+        ordering = ('department_name',)
+
+    def __str__(self):
+        return self.department_name
+
+    def __unicode__(self):
+        return self.department_name
+
 
 class Student(models.Model):
     student_id = models.AutoField(max_length=11,null=False, primary_key = True)
     student_name = models.CharField(max_length=250, blank=False)
     student_email = models.EmailField(max_length=250)
     student_dob = models.DateField()
-    # studend_dob = models.DateField()
+    department_id = models.ForeignKey(Department,on_delete= models.CASCADE)
 
     class Meta:
         ordering = ('student_id',)
@@ -18,16 +31,3 @@ class Student(models.Model):
     def __unicode__(self):
         return str(self.student_id)
 
-class Department(models.Model):
-    department_id = models.AutoField(primary_key = True)
-    student_id = models.ForeignKey(Student, on_delete= models.CASCADE)
-    department_name = models.CharField(max_length=250, blank=False)
-
-    class Meta:
-        ordering = ('department_name',)
-
-    def __str__(self):
-        self.department_name
-
-    def __unicode__(self):
-        self.department_name
